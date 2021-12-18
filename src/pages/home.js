c0132261b1ba16a2f4791e33294b0867e17cf803
AdeebIsmail <adeebi5412@gmail.com>
2021-12-17 22:20:14 -0600

    home

--- src/pages/home.js
+++ src/pages/home.js
@@ -35,8 +35,6 @@ function home(props) {
   return (
     <div
       style={{
-        display: "flex",
-        flexDirection: "column",
         backgroundColor: "#2b2828",
         minHeight: "100vh",
         marginBottom: "100px",
@@ -91,81 +89,85 @@ function home(props) {
             </Carousel>
           </div>
         </div>
-
-        <div
-          style={{
-            marginTop: "1rem",
-          }}
-        >
-          <MDBContainer>
-            {" "}
-            <MDBRow>
-              <MDBCol style={{ marginTop: "1rem" }}>
-                {" "}
-                <div
-                  class="card text-center"
-                  style={{ backgroundColor: "#1E1E1E" }}
-                >
-                  <div class="card-header"></div>
-                  <div class="card-body" style={{ color: "white" }}>
-                    <h5 class="card-title" style={{ color: "white" }}>
-                      News
-                    </h5>
-                    <div className="mission">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                      Sed sit amet metus vitae enim tempus suscipit nec nec
-                      metus. Ut sed urna eros. Nam massa est, maximus a mi eget,
-                      hendrerit egestas elit. Mauris convallis tellus sit amet
-                      posuere maximus. Donec arcu augue, vulputate sed ante in,
-                      tincidunt luctus nibh. Aenean lorem quam, aliquet in dolor
-                      sed, volutpat sagittis nisi. Nam eu felis nec neque
-                      vulputate feugiat. Vivamus sodales neque orci, vel
-                      consectetur mi pretium ut. Quisque ornare volutpat elit.
-                      Maecenas luctus ante mauris, id varius metus imperdiet
-                      vel. Cras rutrum ante nec tristique auctor. Pellentesque
-                      faucibus iaculis est, et rutrum lorem vehicula eu. Nunc
-                      dictum augue vel lobortis tincidunt. Vivamus et blandit
-                      eros. Aliquam non eros sit amet ligula cursus cursus at
-                      nec lectus. Proin viverra mi vel volutpat pellentesque.
-                      Suspendisse vulputate dapibus volutpat. Ut sed odio
-                      volutpat, aliquam sapien vehicula, mollis eros. Praesent
-                      dictum turpis a dolor mollis, at congue tortor imperdiet.
-                      Proin condimentum, mauris nec vulputate posuere, nisl
-                      ligula fermentum lectus, eu pulvinar odio nisl vitae ante.
-                      In hac habitasse platea dictumst. Pellentesque a mollis
-                      magna. Nam pharetra urna viverra erat suscipit ultrices.
-                      Interdum et malesuada fames ac ante ipsum primis in
-                      faucibus. Donec ullamcorper et metus quis bibendum. Ut
-                      eget tellus eu risus lacinia ullamcorper ut vitae ipsum.
-                      Fusce viverra venenatis est nec maximus. Pellentesque
-                      sodales ut tellus sit amet gravida. Donec rutrum sapien
-                      ipsum, eget tristique ante facilisis ut. In urna purus,
-                      suscipit ac tellus egestas, egestas tempus ex. Etiam
-                      accumsan egestas nisi eu sodales. Vivamus sit amet nibh et
-                      risus fringilla fermentum. Maecenas ullamcorper pulvinar
-                      lectus, eu viverra massa auctor in. Quisque non nulla nec
-                      nibh aliquet gravida. Ut vestibulum tortor tortor, quis
-                      porttitor sapien tincidunt sit amet. Aenean vitae mi vitae
-                      ex ornare vehicula. Integer eget diam vitae est mattis
-                      varius ut vitae justo. Proin at quam vitae ex consequat
-                      commodo non at velit. Sed placerat nunc non nunc lacinia,
-                      eget gravida diam rhoncus. Donec ut neque at mi porttitor
-                      volutpat vel finibus lacus. In venenatis leo eu velit
-                      condimentum, sed convallis ligula laoreet. Duis porttitor
-                      ultricies diam, tempus dignissim sapien tincidunt quis.
-                      Quisque non varius arcu. Nulla vehicula neque a leo
-                      consectetur tincidunt. Integer ut mattis mi, non egestas
-                      eros.{" "}
-                    </div>
-                  </div>
-                  <div class="card-footer text-muted"></div>
-                </div>
-              </MDBCol>
-            </MDBRow>
-          </MDBContainer>
-        </div>
-        <Footer />
       </div>
+      <div
+        style={{
+          marginTop: "1rem",
+        }}
+      >
+        <MDBContainer>
+          {" "}
+          <MDBRow>
+            <MDBCol
+              style={{
+                marginTop: "1rem",
+              }}
+            >
+              {" "}
+              <div
+                class="card text-center"
+                style={{
+                  backgroundColor: "#1E1E1E",
+                }}
+              >
+                <div class="card-header"></div>
+                <div class="card-body" style={{ color: "white" }}>
+                  <h5 class="card-title" style={{ color: "white" }}>
+                    News
+                  </h5>
+                  <div className="mission">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                    sit amet metus vitae enim tempus suscipit nec nec metus. Ut
+                    sed urna eros. Nam massa est, maximus a mi eget, hendrerit
+                    egestas elit. Mauris convallis tellus sit amet posuere
+                    maximus. Donec arcu augue, vulputate sed ante in, tincidunt
+                    luctus nibh. Aenean lorem quam, aliquet in dolor sed,
+                    volutpat sagittis nisi. Nam eu felis nec neque vulputate
+                    feugiat. Vivamus sodales neque orci, vel consectetur mi
+                    pretium ut. Quisque ornare volutpat elit. Maecenas luctus
+                    ante mauris, id varius metus imperdiet vel. Cras rutrum ante
+                    nec tristique auctor. Pellentesque faucibus iaculis est, et
+                    rutrum lorem vehicula eu. Nunc dictum augue vel lobortis
+                    tincidunt. Vivamus et blandit eros. Aliquam non eros sit
+                    amet ligula cursus cursus at nec lectus. Proin viverra mi
+                    vel volutpat pellentesque. Suspendisse vulputate dapibus
+                    volutpat. Ut sed odio volutpat, aliquam sapien vehicula,
+                    mollis eros. Praesent dictum turpis a dolor mollis, at
+                    congue tortor imperdiet. Proin condimentum, mauris nec
+                    vulputate posuere, nisl ligula fermentum lectus, eu pulvinar
+                    odio nisl vitae ante. In hac habitasse platea dictumst.
+                    Pellentesque a mollis magna. Nam pharetra urna viverra erat
+                    suscipit ultrices. Interdum et malesuada fames ac ante ipsum
+                    primis in faucibus. Donec ullamcorper et metus quis
+                    bibendum. Ut eget tellus eu risus lacinia ullamcorper ut
+                    vitae ipsum. Fusce viverra venenatis est nec maximus.
+                    Pellentesque sodales ut tellus sit amet gravida. Donec
+                    rutrum sapien ipsum, eget tristique ante facilisis ut. In
+                    urna purus, suscipit ac tellus egestas, egestas tempus ex.
+                    Etiam accumsan egestas nisi eu sodales. Vivamus sit amet
+                    nibh et risus fringilla fermentum. Maecenas ullamcorper
+                    pulvinar lectus, eu viverra massa auctor in. Quisque non
+                    nulla nec nibh aliquet gravida. Ut vestibulum tortor tortor,
+                    quis porttitor sapien tincidunt sit amet. Aenean vitae mi
+                    vitae ex ornare vehicula. Integer eget diam vitae est mattis
+                    varius ut vitae justo. Proin at quam vitae ex consequat
+                    commodo non at velit. Sed placerat nunc non nunc lacinia,
+                    eget gravida diam rhoncus. Donec ut neque at mi porttitor
+                    volutpat vel finibus lacus. In venenatis leo eu velit
+                    condimentum, sed convallis ligula laoreet. Duis porttitor
+                    ultricies diam, tempus dignissim sapien tincidunt quis.
+                    Quisque non varius arcu. Nulla vehicula neque a leo
+                    consectetur tincidunt. Integer ut mattis mi, non egestas
+                    eros.{" "}
+                  </div>
+                </div>
+                <div class="card-footer text-muted"></div>
+              </div>
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
+      </div>
+      <Footer />
     </div>
   );
 }
